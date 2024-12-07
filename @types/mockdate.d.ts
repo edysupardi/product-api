f3012374cbb2f695161ffe349f1bad71ad8db9f7
Edy <46397561+edysupardi@users.noreply.github.com>
2024-12-07 19:20:39 +0700

    fix: test hello world

---
+++ @types/mockdate.d.ts
@@ -0,0 +1,4 @@
+declare module 'mockdate' {
+  export function set(date: string | Date): void;
+  export function reset(): void;
+}
--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import MockDate from 'mockdate';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -14,9 +15,16 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  afterEach(() => {
+    MockDate.reset(); // Reset waktu mock setelah setiap test
+  });
+
+  describe('getHello', () => {
+    it('should return a greeting message with the current UTC time', () => {
+      MockDate.set('2024-12-07T12:15:00Z'); // Set waktu mock
+
+      const result = appController.getHello();
+      expect(result).toBe('API Product v1.0 at Sat, 07 Dec 2024 12:15:00 GMT');
     });
   });
 });
